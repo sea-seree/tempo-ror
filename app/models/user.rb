18ab85a565311fb84312a3737313916e69267805
sea-seree <sea@odds.team>
2024-09-04 11:24:29 +0700

    feat: validation in models User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthday, presence: true
+  validates :gender, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :subject, presence: true
 end
