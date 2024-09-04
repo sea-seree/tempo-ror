e1c54f083bb6282d45c70ff084a860e09bfe8231
sea-seree <sea@odds.team>
2024-09-04 11:30:20 +0700

    feat:add presence, format, and inclusion validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # ตรวจสอบว่าฟิลด์เหล่านี้ต้องมีค่า
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
   validates :birthday, presence: true
-  validates :gender, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
-  validates :subject, presence: true
+  validates :gender, presence: true, inclusion: { in: %w(male female), message: "%{value} is not a valid gender" }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :phone, presence: true, format: { with: /\A[0-9]{10}\z/, message: "must be 10 digits" }
+  validates :subject, presence: true, inclusion: { in: %w(html css js ts), message: "%{value} is not a valid subject" }
 end
