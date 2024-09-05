ecb8972b029cc83ab0d06b585633c81d0fc80340
sea-seree <sea@odds.team>
2024-09-05 11:50:00 +0700

    feat: transition when click to navigate page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,32 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.classList.add('fade-in');
+  
+    setTimeout(function () {
+      document.body.classList.add('fade-in-active');
+    },200);
+  
+    const links = document.querySelectorAll('a[href]');
+    let isTransitioning = false;
+  
+    links.forEach(link => {
+      link.addEventListener('click', function (e) {
+        if (isTransitioning) return;
+        isTransitioning = true;
+  
+        e.preventDefault();
+        const href = this.href;
+  
+        document.body.classList.add('fade-out');
+  
+        setTimeout(function () {
+          window.location.href = href;
+        }, 50);
+      });
+    });
+  });
+  
