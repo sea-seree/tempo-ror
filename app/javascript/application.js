e933a07160ccb58b84fb7fedcf7237323262559c
sea-seree <sea@odds.team>
2024-09-15 15:20:25 +0700

    feat: add thai language

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,30 +3,30 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.body.classList.add('fade-in');
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.body.classList.add('fade-in');
   
-    setTimeout(function () {
-      document.body.classList.add('fade-in-active');
-    },200);
+//     setTimeout(function () {
+//       document.body.classList.add('fade-in-active');
+//     },200);
   
-    const links = document.querySelectorAll('a[href]');
-    let isTransitioning = false;
+//     const links = document.querySelectorAll('a[href]');
+//     let isTransitioning = false;
   
-    links.forEach(link => {
-      link.addEventListener('click', function (e) {
-        if (isTransitioning) return;
-        isTransitioning = true;
+//     links.forEach(link => {
+//       link.addEventListener('click', function (e) {
+//         if (isTransitioning) return;
+//         isTransitioning = true;
   
-        e.preventDefault();
-        const href = this.href;
+//         e.preventDefault();
+//         const href = this.href;
   
-        document.body.classList.add('fade-out');
+//         document.body.classList.add('fade-out');
   
-        setTimeout(function () {
-          window.location.href = href;
-        }, 50);
-      });
-    });
-  });
+//         setTimeout(function () {
+//           window.location.href = href;
+//         }, 50);
+//       });
+//     });
+//   });
   
